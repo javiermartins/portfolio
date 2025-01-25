494cf7222e5b1f8e20c039c3d06930c32ac83768
javiermartins <javiimartinserrano@gmail.com>
2025-01-25 20:09:14 +0100

    New project and other projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,10 +1,12 @@
 import Angular from "../components/icons/tags/Angular.astro";
 import Appwrite from "../components/icons/tags/Appwrite.astro";
 import AstroLogo from "../components/icons/tags/Astro.astro";
+import CloudflareWorkers from "../components/icons/tags/CloudflareWorkers.astro";
 import Firebase from "../components/icons/tags/Firebase.astro";
 import Ionic from "../components/icons/tags/Ionic.astro";
 import TaigaUI from "../components/icons/tags/TaigaUI.astro";
 import Tailwind from "../components/icons/tags/Tailwind.astro";
+import Turso from "../components/icons/tags/Turso.astro";
 import type { Project } from "../types/project";
 
 const TAGS: any = {
@@ -19,6 +21,10 @@ const TAGS: any = {
     ASTRO: {
         name: "Astro",
         icon: AstroLogo
+    },
+    CLOUDFLAREWORKERS: {
+        name: "Cloudflare workers",
+        icon: CloudflareWorkers
     },
     FIREBASE: {
         name: "Firebase",
@@ -36,24 +42,36 @@ const TAGS: any = {
         name: "Tailwind",
         icon: Tailwind
     },
+    TURSO: {
+        name: "Turso",
+        icon: Turso
+    }
 };
 
 const PROJECTS: Project[] = [
-    {
-        title: "Ordo",
-        description: "PROJECTS.ORDODESC",
-        url: "",
-        githubLink: "https://github.com/javiermartins/ordo",
-        image: "https://via.placeholder.com/300",
-        tags: [TAGS.ANGULAR, TAGS.TAIGAUI, TAGS.FIREBASE, TAGS.TAILWIND],
-    },
+    /*     {
+            title: "Ordo",
+            description: "PROJECTS.ORDODESC",
+            url: "",
+            githubLink: "https://github.com/javiermartins/ordo",
+            image: "",
+            tags: [TAGS.ANGULAR, TAGS.TAIGAUI, TAGS.FIREBASE, TAGS.TAILWIND],
+        }, */
     {
         title: "Cutlynk",
         description: "PROJECTS.CUTLYNKDESC",
-        url: "https://cutlynk.vercel.app/",
+        url: "https://cutlynk.dev",
         githubLink: "https://github.com/javiermartins/cutlynk",
         image: "img/projects/cutlynk.webp",
         tags: [TAGS.ANGULAR, TAGS.TAIGAUI, TAGS.APPWRITE, TAGS.TAILWIND],
+    },
+    {
+        title: "Factos",
+        description: "PROJECTS.FACTOSDESC",
+        url: "https://factos.pages.dev/",
+        githubLink: "https://github.com/javiermartins/factos",
+        image: "img/projects/factos.webp",
+        tags: [TAGS.ASTRO, TAGS.CLOUDFLAREWORKERS, TAGS.TURSO, TAGS.TAILWIND],
     },
     {
         title: "Tastime",
