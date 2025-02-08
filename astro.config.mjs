a71bfe2c94d3a571966f78178f8837bb4e9c1d66
javiermartins <javiimartinserrano@gmail.com>
2025-02-08 18:55:54 +0100

    Robots and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
+import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: 'https://javiermartins.dev',
+  integrations: [tailwind(), sitemap()],
   i18n: {
     defaultLocale: 'es',
     locales: ['es', 'en'],
